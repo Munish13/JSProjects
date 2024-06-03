0a85437c4fea4a043a047963dba3150d3aaae33f
Munish Kumar <149750616+Munish13@users.noreply.github.com>
2024-06-03 14:36:43 +0530

    number issue fixed

--- Telephone Formatter/script.js
+++ Telephone Formatter/script.js
@@ -1,9 +1,17 @@
 const telInput = document.getElementById("telephone");
 
+let previousValue = "";
+let firstThreeNum = "";
+
 telInput.addEventListener("input", (e) => {
     const inputVal = e.target.value;
     console.log(inputVal);
-    if (inputVal.length === 4) {
-        telInput.value = `+(${inputVal.substring(0, 3)}) - ${inputVal[inputVal.length - 1]}`
+    if (inputVal.length === 4 && previousValue.length < inputVal.length) {
+        firstThreeNum = inputVal.substring(0, 3);
+        telInput.value = `+(${firstThreeNum}) - ${inputVal[inputVal.length - 1]}`
+    } else if (inputVal.length === 9 && previousValue.length > inputVal.length) {
+        telInput.value = firstThreeNum;
     }
+
+    previousValue = inputVal;
 });
